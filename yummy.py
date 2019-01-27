df10562280b8b3e1f4a1d810dfd168a6f7f3ac35
feihu1996x <gsx0312@qq.com>
2019-01-27 08:45:35 +0800

    蓝图路由规划

--- yummy.py
+++ yummy.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, Blueprint
+
+
+route_yummy = Blueprint("yummy_page", __name__, url_prefix='/yummy')
+
+
+@route_yummy.route("/")
+def index():
+    return "yummy index page"
+
+
+@route_yummy.route("/hello")
+def hello():
+    return "yummy hello page"
+
 
 app = Flask(__name__)
-
-
-@app.route('/yummy/')
-def hello_world():
-    return 'Hello World!'
+app.register_blueprint(route_yummy)
 
 
 if __name__ == '__main__':
