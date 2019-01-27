f8fd43a96b17c34e5ff4862b239be80d750e4e6e
feihu1996x <gsx0312@qq.com>
2019-01-27 10:14:36 +0800

    链接管理器 静态版本号管理 update

--- yummy.py
+++ yummy.py
@@ -1,4 +1,15 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, url_for
+
+
+class UrlManager:
+    @staticmethod
+    def build_url(path):
+        return path
+
+    @staticmethod
+    def build_static_url(path):
+        path = path + '?version=' + "201901270953"
+        return UrlManager.build_url(path)
 
 
 route_yummy = Blueprint("yummy_page", __name__, url_prefix='/yummy')
@@ -11,7 +22,10 @@ def index():
 
 @route_yummy.route("/hello")
 def hello():
-    return "yummy hello page"
+    url = url_for('yummy_page.index')
+    url_1 = UrlManager.build_url('/yummy/')
+    url_2 = UrlManager.build_static_url('/yummy/css/bootstrap.css')
+    return "yummy hello page, %s, %s, %s" % (url, url_1, url_2)
 
 
 app = Flask(__name__)
